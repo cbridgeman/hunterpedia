538ac822cf23aaa2c091af12ef8471448a8bc359
Chad Bridgeman <chad.bridgeman@force5solutions.com>
2014-09-03 22:19:57 -0400

    --new project!

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Chad Bridgeman\Projects\phoneGap\Hunterpedia\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Chad Bridgeman\Projects\phoneGap\Hunterpedia\platforms\android\CordovaLib\AndroidManifest.xml \
